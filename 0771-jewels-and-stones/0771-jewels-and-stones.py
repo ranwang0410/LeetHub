1ba7c5641d6d6809e1e9f64b4787f03bd78d130f
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-05-10 08:46:33 -0700

    Time: 29 ms (91.25%), Space: 16.3 MB (99.32%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewel_set = set(jewels)
+        return sum(stone in jewel_set for stone in stones)
+        
+        
+        
+        
