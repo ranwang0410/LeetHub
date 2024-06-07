d244fec59a1f4e41ec88fa8364c60e59ad52d50d
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-06-07 09:15:17 -0700

    Time: 212 ms (85.18%), Space: 16.4 MB (49.38%) - LeetHub

---
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
@@ -0,0 +1,25 @@
+class TwoSum(object):
+
+    def __init__(self):
+        self.nums={}
+
+    def add(self, number):
+        if number in self.nums:
+            self.nums[number]+=1
+        else:
+            self.nums[number]=1
+
+    def find(self, value):
+        for i in self.nums:
+            if value-i in self.nums:
+                if i==value-i and self.nums[i]==1:
+                    continue
+                return True
+        return False
+        
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
