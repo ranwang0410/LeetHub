c39ed4e96f0a29bf0606f717d09ea1befc15721a
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-06-19 21:34:57 -0700

    Time: 604 ms (86.99%), Space: 13.9 MB (27.85%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def distributeCandies(self, candyType):
+        """
+        :type candyType: List[int]
+        :rtype: int
+        """
+        return min(len(candyType)/2, len(set(candyType)))
+        
