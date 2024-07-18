404756867a9973e711492d1f584818256110a7d2
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-07-17 19:51:39 -0700

    Time: 0 ms (100.00%), Space: 11.8 MB (13.68%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = [root]
+        while stack:
+            currentNode = stack.pop()
+            if currentNode:
+                res.append(currentNode.val)
+                stack.append(currentNode.right)
+                stack.append(currentNode.left)
+        return res
+        
