62ba7d5c9a7d3fb975963506b805b74760052e2e
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-07-19 18:49:57 -0700

    Time: 30 ms (57.82%), Space: 11.7 MB (27.21%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        a=0
+        while a<len(bits):
+            if bits[a]==1:
+                a+=2
+            else:
+                if a==len(bits)-1:
+                    return True
+                else:
+                    a+=1
+        return False
