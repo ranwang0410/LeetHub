f3e864e9056feca31d88b971f4e63a0b22a49838
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-08-07 21:34:14 -0700

    Time: 0 ms (100.00%), Space: 40.7 MB (10.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int l = 0, h = num, mid = 0;
+        while (l <= h) {
+            mid = l + (h - l) / 2;
+            long pow = (long)mid * mid;
+            if (pow == num)
+                return true;
+            else if (pow < num)
+                l = mid + 1;
+            else
+                h = mid - 1;
+        }
+        return false;
+    }
+}
