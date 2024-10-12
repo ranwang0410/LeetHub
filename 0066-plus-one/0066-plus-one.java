19a24cf59deb5d29781a67e2a7b7ad7c829ac529
Ran Wang <135944410+ranwang0410@users.noreply.github.com>
2024-10-12 13:18:02 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (67.10%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-
-        for(int i = n-1; i >= 0; i--){
+        for (int i = digits.length - 1; i >= 0; i--) {
             digits[i]++;
 
-            if(digits[i] < 10){
+            if (digits[i] < 10) {
                 return digits;
-            } else {
-                digits[i] = 0;
             }
+
+            digits[i] = 0;
         }
 
-        int[] newDigits = new int[n+1];
-        newDigits[0] = 1;
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;  
         return newDigits;
     }
 }
